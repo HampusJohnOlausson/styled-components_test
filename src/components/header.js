4a680d4045fd4886836374c6cc815e3cc488785a
hampus <hampus.john.olausson@gmail.com>
2021-10-30 15:23:26 +0200

    implemented button with styled-componenets

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import { StyledHeader } from './styles/Header.styled'
+import { Container } from './styles/container.styled'
+import { StyledHeader, Nav } from './styles/Header.styled'
+import { Button } from './styles/Button.styled'
 
 const Header = () => {
   return (
     <StyledHeader>
-      <h1>Header</h1>
+      <Container>
+        <Nav>
+          <h1>Huddle</h1>
+        <Button>Try It Free</Button>
+        </Nav>
+      </Container>
     </StyledHeader>
   )
 }
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -1,6 +1,12 @@
-import Styled from 'styled-components';
+import styled from 'styled-components';
 
-export const StyledHeader = Styled.header`
+export const StyledHeader = styled.header`
    background-color: ${({ theme }) => theme.colors.header};
+`
 
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center; 
+  justify-content: space-between; 
+  margin-bottom: 40px;
 `
