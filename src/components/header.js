9a0020c33d88228d228f23c1dfbaca059c227325
hampus <hampus.john.olausson@gmail.com>
2021-10-31 10:25:25 +0100

    Implemented global responivness by using theme

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container } from './styles/container.styled'
 import { StyledHeader, Nav } from './styles/Header.styled'
 import { Button } from './styles/Button.styled'
+import { Flex } from './styles/Flex.styled'
 
 const Header = () => {
   return (
@@ -11,6 +12,21 @@ const Header = () => {
           <h1>Huddle</h1>
         <Button>Try It Free</Button>
         </Nav>
+        <Flex>
+          <div>
+            <h1>
+              BUILD THE COMMUNITY
+            </h1>
+            <p>
+             random text random text
+            </p>
+            <Button bg="#ff0099" color="#fff">
+              Get Started for free
+            </Button>
+          </div>
+
+
+        </Flex>
       </Container>
     </StyledHeader>
   )
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -9,4 +9,8 @@ export const Nav = styled.nav`
   align-items: center; 
   justify-content: space-between; 
   margin-bottom: 40px;
+
+  @media(max-width: ${({ theme }) => theme.mobile}) {
+    flex-direction: column;
+  }
 `
